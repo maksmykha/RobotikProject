1b644ba0a5628df56c29ece020e0272802a006df
maksmykha <mykhailich@gmail.com>
2026-05-24 07:53:07 +0000

    added Förderband

---
+++ src/robotik_gazebo/launch/gazebo_world.launch.py
@@ -0,0 +1,21 @@
+from launch import LaunchDescription
+from launch.actions import ExecuteProcess
+from ament_index_python.packages import get_package_share_directory
+import os
+
+
+def generate_launch_description():
+    pkg_robotik_gazebo = get_package_share_directory('robotik_gazebo')
+
+    world_file = os.path.join(
+        pkg_robotik_gazebo,
+        'worlds',
+        'pick_place_world.sdf'
+    )
+
+    return LaunchDescription([
+        ExecuteProcess(
+            cmd=['gz', 'sim', world_file],
+            output='screen'
+        )
+    ])

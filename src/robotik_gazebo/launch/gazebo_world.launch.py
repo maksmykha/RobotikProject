2855ca89579d22df78d05d961419eba547d83f0e
Tim <timbor18@gmail.com>
2026-06-06 16:09:28 +0200

    Improved Launch Script

--- src/robotik_gazebo/launch/gazebo_world.launch.py
+++ src/robotik_gazebo/launch/gazebo_world.launch.py
@@ -5,9 +5,12 @@ import os
 
 
 def generate_launch_description():
+
     pkg_robotik_gazebo = get_package_share_directory('robotik_gazebo')
     pkg_robotik_vision = get_package_share_directory('robotik_vision')
-    model_path = os.path.join(pkg_robotik_vision, 'models')
+
+    # Falls Modelle
+    model_path = os.path.join(pkg_robotik_gazebo, 'models')
 
     world_file = os.path.join(
         pkg_robotik_gazebo,
@@ -15,17 +18,29 @@ def generate_launch_description():
         'pick_place_world.sdf'
     )
 
+    # Gazebo Resource Path setzen
     set_gz_model_path = SetEnvironmentVariable(
-    name='GZ_SIM_RESOURCE_PATH',
-    value=model_path + ":" + os.environ.get("GZ_SIM_RESOURCE_PATH", "")
-)
+        name='GZ_SIM_RESOURCE_PATH',
+        value=model_path + ":" + os.environ.get("GZ_SIM_RESOURCE_PATH", "")
+    )
 
+    # Gazebo starten
     gazebo = ExecuteProcess(
         cmd=['gz', 'sim', world_file],
         output='screen'
     )
 
+    #ROS2  Gazebo Bridge für Kamera
+    camera_bridge = ExecuteProcess(
+        cmd=[
+            'ros2', 'run', 'ros_gz_bridge', 'parameter_bridge',
+            '/gripper_camera/image@sensor_msgs/msg/Image@gz.msgs.Image'
+        ],
+        output='screen'
+    )
+
     return LaunchDescription([
         set_gz_model_path,
-        gazebo
+        gazebo,
+        camera_bridge
     ])

8f10b15ecaee9153e8caedf15f0d840dcb6730f4
maksmykha <mykhailich@gmail.com>
2026-06-25 20:16:14 +0000

    feat: MoveIt + Gazebo fully working - arm moves on Plan & Execute

--- src/robotik_vision/setup.py
+++ src/robotik_vision/setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
-from glob import glob
 import os
+from glob import glob
 
 package_name = 'robotik_vision'
 
@@ -12,31 +12,15 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-   	(
-    	os.path.join(
-        	'share',
-        	package_name,
-        	'models',
-        	'gripper_depth_camera'
-    	),
-    	glob('models/gripper_depth_camera/*')
-	), ],
+        ('share/' + package_name + '/models/gripper_depth_camera',
+            glob('models/gripper_depth_camera/*')),
+    ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='maksmykha',
     maintainer_email='mykhailich@gmail.com',
     description='TODO: Package description',
     license='TODO: License declaration',
-    extras_require={
-        'test': [
-            'pytest',
-        ],
-    },
-    entry_points={
-    'console_scripts': [
-        'color_detector_node = robotik_vision.color_detector_node:main',
-        'manual_target_selector_node = robotik_vision.manual_target_selector_node:main',
-        'target_pose_node = robotik_vision.target_pose_node:main',
-    ],
-},
+    extras_require={'test': ['pytest']},
+    entry_points={'console_scripts': []},
 )
